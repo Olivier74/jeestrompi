6894108d0e77211be86b0348a1934b8818c4c32f
Olivier74 <github@zago.fr>
2024-03-13 10:53:31 +0100

    update

--- core/class/jee-strompi.class.php
+++ core/class/jee-strompi.class.php
@@ -18,7 +18,7 @@
 /* * ***************************Includes********************************* */
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
-class template extends eqLogic {
+class jee-strompi extends eqLogic {
   /*     * *************************Attributs****************************** */
 
   /*
@@ -148,7 +148,7 @@ class template extends eqLogic {
   /*     * **********************Getteur Setteur*************************** */
 }
 
-class templateCmd extends cmd {
+class jee-strompiCmd extends cmd {
   /*     * *************************Attributs****************************** */
 
   /*
