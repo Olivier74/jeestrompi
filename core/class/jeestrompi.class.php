2f8875091017ad9beeaf2f41fb2954db9f229565
Olivier74 <github@zago.fr>
2024-03-18 10:45:07 +0100

    update1803-1045

--- core/class/jeestrompi.class.php
+++ core/class/jeestrompi.class.php
@@ -17,12 +17,10 @@
 
 /* * ***************************Includes********************************* */
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
-/*require_once __DIR__ . '/../../3rdparty/tomitomas/tomitomasEqLogicTrait.php';*/
-
 
 class jeestrompi extends eqLogic {
   /*     * *************************Attributs****************************** */
-	use tomitomasEqLogicTrait;
+
   /*
   * Permet de définir les possibilités de personnalisation du widget (en cas d'utilisation de la fonction 'toHtml' par exemple)
   * Tableau multidimensionnel - exemple: array('custom' => true, 'custom::layout' => false)
@@ -34,7 +32,7 @@ class jeestrompi extends eqLogic {
   * Exemple : "param1" & "param2" seront cryptés mais pas "param3"
   public static $_encryptConfigKey = array('param1', 'param2');
   */
-	
+
   /*     * ***********************Methode static*************************** */
 	 public static function deamon_info() {
         $return = array();
@@ -233,6 +231,116 @@ class jeestrompi extends eqLogic {
     $strompiarray = array($StromPiMode,$StromPiLifePo4V,$StromPiWide,$StromPiUSB,$StromPiLifePo4Charge,$StromPiOutput);
     return $strompiarray;
   }
+  
+  public function createCommands(string $file, string $type) {
+        $configFile = self::getFileContent($file);
+        $dict = $configFile['dictionary'];
+        try {
+            if (isset($configFile['cmds'][$type])) {
+                $this->createCommandsFromConfigFile($configFile['cmds'][$type], $dict);
+            } else {
+                self::error($type . ' not found in config');
+            }
+        } catch (Exception $e) {
+            self::error('Cannot save Cmd for this EqLogic -- ' . $e->getMessage());
+        }
+    }
+  
+  public static function getFileContent($path) {
+
+        if (!file_exists($path)) {
+            self::error('File not found  : ' . $path);
+            return null;
+        }
+
+        $content = file_get_contents($path);
+
+        if (is_json($content)) {
+            return json_decode($content, true);
+        }
+
+        return $content;
+    }
+  
+  public function createCommandsFromConfigFile($commands, $dict) {
+        $cmd_updated_by = array();
+        foreach ($commands as $cmdData) {
+            $cmd = $this->getCmd(null, $cmdData["logicalId"]);
+
+            if (!is_object($cmd)) {
+                self::debug('cmd creation => ' . $cmdData["name"] . ' [' . $cmdData["logicalId"] . ']');
+
+                $cmd = new cmd();
+                $cmd->setLogicalId($cmdData["logicalId"]);
+                $cmd->setEqLogic_id($this->getId());
+
+                if (isset($cmdData["isVisible"])) {
+                    $cmd->setIsVisible($cmdData["isVisible"]);
+                }
+
+                if (isset($cmdData["isHistorized"])) {
+                    $cmd->setIsHistorized($cmdData["isHistorized"]);
+                }
+
+                if (isset($cmdData["generic_type"])) {
+                    $cmd->setGeneric_type($cmdData["generic_type"]);
+                }
+
+                if (isset($cmdData["unite"])) {
+                    $cmd->setUnite($cmdData["unite"]);
+                }
+
+                if (isset($cmdData["order"])) {
+                    $cmd->setOrder($cmdData["order"]);
+                }
+            }
+
+            $cmd->setName(__($cmdData["name"], __FILE__));
+
+            $cmd->setType($cmdData["type"]);
+            $cmd->setSubType($cmdData["subtype"]);
+
+            if (isset($cmdData['configuration'])) {
+                foreach ($cmdData['configuration'] as $key => $value) {
+                    if ($key == 'listValueToCreate') {
+                        $key = 'listValue';
+                        $value = self::createListOption(explode(";", $value), $dict);
+                    }
+                    $cmd->setConfiguration($key, $value);
+                }
+            }
+
+            if (isset($cmdData['display'])) {
+                foreach ($cmdData['display'] as $key => $value) {
+                    $cmd->setDisplay($key, $value);
+                }
+            }
+
+            if (isset($cmdData['template'])) {
+                foreach ($cmdData['template'] as $key => $value) {
+                    $cmd->setTemplate($key, $value);
+                }
+            }
+
+            if (isset($cmdData['updateCmd'])) {
+                $cmd_updated_by[$cmdData["logicalId"]] = $cmdData['updateCmd'];
+            }
+
+            $cmd->save();
+        }
+
+        foreach ($cmd_updated_by as $cmdAction_logicalId => $cmdInfo_logicalId) {
+            $cmdAction = $this->getCmd(null, $cmdAction_logicalId);
+            $cmdInfo = $this->getCmd(null, $cmdInfo_logicalId);
+
+            if (is_object($cmdAction) && is_object($cmdInfo)) {
+                $cmdAction->setValue($cmdInfo->getId());
+                $cmdAction->save();
+            }
+        }
+    }
+  
+  
   /*
   * Fonction exécutée automatiquement toutes les heures par Jeedom
   public static function cronHourly() {}
